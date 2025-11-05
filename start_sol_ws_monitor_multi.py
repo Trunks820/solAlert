a862f3d394b78434cb9b81c33868b10a57ce4ea3
KAKAROT8 <Kakarot820@outlook.com>
2025-11-05 14:05:48 +0800

    优化ws链接

--- start_sol_ws_monitor_multi.py
+++ start_sol_ws_monitor_multi.py
@@ -189,10 +189,10 @@ async def batch_ws_handler(
         try:
             logger.info(f"🔌 [{conn_name}] 正在连接 {WS_URL}...")
             
-            # websockets 12.0+ 使用 additional_headers
+            # websockets 12.0 使用 extra_headers
             async with websockets.connect(
                 WS_URL,
-                additional_headers={'x-api-key': API_KEY},
+                extra_headers={'x-api-key': API_KEY},
                 ping_interval=30,
                 ping_timeout=60,
                 close_timeout=10
